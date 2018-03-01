88df98d8717b5d09fd289ff4b89f622219384240
Michael Wang <m806w839@ku.edu>
2018-03-01 14:54:03 -0600

    added a main.cpp

---
+++ hashTable/main.cpp
@@ -0,0 +1,11 @@
+/*
+*	Name: Michael Wang
+*	Program Name: hashTable
+*	Description: This is a hashtable implementation
+*/
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	return(0);	
+}
